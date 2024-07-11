ba57aed2e4053d4ed7301d587fefdd10913fec8e
Valabor <valerii.aborniev@gmail.com>
2024-07-11 19:27:34 +0200

    fixed update issues

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
 
-import { AppModule } from './app/app.module';
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes)]
+};
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
